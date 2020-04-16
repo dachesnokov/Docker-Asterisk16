3d29df88a8f317c4c8ab13201695fd971246e321
dachesnokov <dachesnokov@outlook.com>
2020-04-16 05:02:39 +0300

    Update start_aster.sh

--- centos7/start_aster.sh
+++ centos7/start_aster.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-chown -R asterisk:asterisk /etc/asterisk/
-chown -R asterisk:asterisk /var/{lib,log,spool}/asterisk
-chown -R asterisk:asterisk /usr/lib64/asterisk/
 asterisk -U asterisk
 sleep 5
 asterisk -r
