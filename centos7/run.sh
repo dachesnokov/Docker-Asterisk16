6f2e604029470f146f950c93b88020edb3f38769
dachesnokov <dachesnokov@outlook.com>
2020-04-16 18:04:53 +0300

    Update run.sh

--- centos7/run.sh
+++ centos7/run.sh
@@ -4,7 +4,7 @@ docker run \
  -v /var/docker/Docker-Asterisk16/centos7/var/log/asterisk:/var/log/asterisk \
  --name pbx \
  -h pbx \
- -p 10000-20000:10000-20000/udp \
+ -p 10000-10150:10000-10150/udp \
  -p 5060:5060/udp \
  -p 5061:5061/udp \
  --restart=always \
