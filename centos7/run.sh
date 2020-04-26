abffc4f665fdfb8efa641c7dc8c904d49fc0941a
dachesnokov <dachesnokov@outlook.com>
2020-04-26 23:30:48 +0300

    Update run.sh

--- centos7/run.sh
+++ centos7/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker run \
- -v /var/docker/Docker-Asterisk16/centos7/etc/asterisk:/etc/asterisk \
- -v /var/docker/Docker-Asterisk16/centos7/var/log/asterisk:/var/log/asterisk \
+ -v ./etc/asterisk:/etc/asterisk \
+ -v ./var/log/asterisk:/var/log/asterisk \
  --name pbx \
  -h pbx \
  --net=host \
